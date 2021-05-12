1ddada1b22b96415eb3d2d24dfd3f56c0d330a4b
patrickliu022 <patrickliu022@gmail.com>
2021-05-12 15:19:31 -0700

    reorganizing files

---
+++ src/App/Landing.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Landing.css';
+
+function Landing() {
+
+    return (
+        <div className='body'>
+
+        </div>
+    )
+}
+
+function Banner() {
+    return (
+        <div className='title-banner'>
+            <img src='./img/EBA_title_img.jpeg' alt='Emma B. Andrews title image' />
+        </div>
+    )
+}
+
+
+
+export default Landing;
