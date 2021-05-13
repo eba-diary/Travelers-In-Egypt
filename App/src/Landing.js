862f136e8b666409e5219311519d2b72e284e581
patrickliu022 <patrickliu022@gmail.com>
2021-05-13 00:33:52 -0700

    added navigation cards to landing page

--- App/src/Landing.js
+++ App/src/Landing.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import './Landing.css';
 import ebaTitle from './img/EBA_title_img.jpeg';
+import personalArchives from './img/personalArchives.png';
+import travelogues from './img/Travelogues.png';
+import tourOperators from './img/TourOperators.png';
+import digitalExhibit from './img/DigitalExhibit.png';
+import about from './img/About.png';
 
 function Landing() {
-
+    const IMAGES = {
+        card1: [personalArchives, 'Personal Archives'],
+        card2: [travelogues, 'Published Travelogues'], 
+        card3: [tourOperators, 'Tour Operators'], 
+        card4: [digitalExhibit, 'Digital Exhibit'], 
+        card5: [about, 'About']
+    };
+    
     return (
         <div className='body'>
             <Banner/>
+            <NavCards images={IMAGES}/>
         </div>
-    )
+    );
 }
 
 function Banner() {
@@ -30,5 +43,33 @@ function Banner() {
     )
 }
 
+function NavCards(props) {
+    return (
+        <div className='card-container'>
+                <RenderCards images={props.images}/>
+        </div>
+    )
+}
+
+function RenderCards(props) {
+    let cards = Object.values(props.images).map(([path, desc]) => {
+        let card = 
+            <div className='each-card'>
+                <div className='top-card'>
+                    <img src={path} alt={desc} />
+                    <div className='bottom-card'>
+                        <p>{desc}</p>
+                    </div>
+                </div> 
+            </div>;
+        return card;
+    });
+
+    return (
+        <div className='card-row'>
+            {cards}
+        </div>
+    );
+}
 
 export default Landing;
