d8e0f2523a0765977823c2caee2a1b169f365e37
patrickliu022 <patrickliu022@gmail.com>
2021-05-12 18:12:53 -0700

    created responsive title banner

--- App/src/Landing.js
+++ App/src/Landing.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Landing.css';
+import ebaTitle from './img/EBA_title_img.jpeg';
 
 function Landing() {
 
     return (
         <div className='body'>
-
+            <Banner/>
         </div>
     )
 }
@@ -13,7 +14,18 @@ function Landing() {
 function Banner() {
     return (
         <div className='title-banner'>
-            <img src='./img/EBA_title_img.jpeg' alt='Emma B. Andrews title image' />
+            <div className='container'>
+                <div className='title-box'>
+                    <img src={ebaTitle} alt='Emma B. Andrews title'/>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='image-box'>
+                    <div className='header-box'>
+                        <h1>Travelers In Egypt</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
