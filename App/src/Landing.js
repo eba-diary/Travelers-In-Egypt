8e83a696f048b9ab2a61380c60af68739d54001e
patrickliu022 <patrickliu022@gmail.com>
2021-05-13 21:50:03 -0700

    added footer and social links

--- App/src/Landing.js
+++ App/src/Landing.js
@@ -6,6 +6,8 @@ import travelogues from './img/Travelogues.png';
 import tourOperators from './img/TourOperators.png';
 import digitalExhibit from './img/DigitalExhibit.png';
 import about from './img/About.png';
+import cc from './img/cc.png';
+import { SocialIcon } from 'react-social-icons';
 
 function Landing() {
     const IMAGES = {
@@ -17,9 +19,16 @@ function Landing() {
     };
     
     return (
-        <div className='body'>
-            <Banner/>
-            <NavCards images={IMAGES}/>
+        <div className='landing-page'>
+            <div className='header'>
+                <Banner/>
+            </div>
+            <div className='body'>
+                <NavCards images={IMAGES}/>
+            </div>
+            <div className='footer'>
+                <FooterLinks/>
+            </div>
         </div>
     );
 }
@@ -70,6 +79,34 @@ function RenderCards(props) {
             {cards}
         </div>
     );
+}
+
+function FooterLinks() {
+    return (
+        <div className='footer-links'>
+            <div className='social-link-container'>
+                <div className='icon-link'>
+                    <SocialIcon url='https://www.facebook.com/ebadiary' bgColor='rgba(119, 101, 63, 0.8)'/>
+                </div>
+                <div className='icon-link'>
+                    <SocialIcon url='https://github.com/orgs/eba-diary/dashboard' bgColor='rgba(119, 101, 63, 0.8)'/>
+                </div>
+                <div className='icon-link'>
+                    <SocialIcon url='https://twitter.com/ebadiary?lang=en' bgColor='rgba(119, 101, 63, 0.8)'/>
+                </div>
+            </div>
+            <div className='cc-info'>
+                <div className='cc-image'>
+                    <img id='cc' src={cc} alt='creative commons logo'/>
+                </div>
+                <div className='cc-link'>
+                    <a href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>
+                        Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Landing;
