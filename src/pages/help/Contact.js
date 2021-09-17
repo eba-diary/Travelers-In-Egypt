553b1745ae8380d71ed0f1a73674bbe11d588d40
patrickliu022 <patrickliu022@gmail.com>
2021-09-17 03:43:10 -0700

    finished contact form

--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -6,30 +6,6 @@ import emailjs from 'emailjs-com';
 // 1. input required field still allows empty messages sent
 // 2. Need to create resizable message box
 
-// function Contact() {
-
-//     const handleClick = () => {
-//         window.open('mailto:emmabandrewsdiaryproject@gmail.com?subject=Inquiry');
-//     }
-//     // Contact form
-//     let form = 
-//         <div className='container'>
-//             <div className='col-12 header'>
-//                 <h1>Contact Us</h1>
-//             </div>
-//             <div>
-//                 <p>
-//                     We are working on an on-site contact form. 
-//                     <span className='mail' onClick={handleClick}> Click here </span>
-//                     to open your mailing client
-//                 </p>
-//             </div>
-//             {/* <Form /> */}
-//         </div>
-
-//     return(form);
-// }
-
 // function Form() {
 
 //     const handleSubmit = (e) => {
@@ -47,9 +23,9 @@ import emailjs from 'emailjs-com';
 //         document.querySelector('.alert').style.display = 'block';
 
 //         // Hide alert after 3 seconds.
-//         setTimeout(() => {
-//             document.querySelector('.alert').style.display = 'none';
-//         }, 3500);
+        // setTimeout(() => {
+        //     document.querySelector('.alert').style.display = 'none';
+        // }, 3500);
 
 //         // Clear the form.
 //         document.getElementById('contactForm').reset();
@@ -79,38 +55,16 @@ import emailjs from 'emailjs-com';
 
 
 function Contact() {
-    console.log(process.env.SERVICE_ID);
-    console.log(process.env.TEMPLATE_ID);
-    console.log(process.env.USER_ID);
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,
-            process.env.REACT_APP_TEMPLATE_ID, 
-            e.target, 
-            process.env.REACT_APP_USER_ID)
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-        e.target.reset()
-    }
 
     return (
-        <form className="contact-form" onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="name" />
-
-            <label>Email</label>
-            <input type="email" name="email" />
-            
-            <label>Message</label>
-            <textarea type="text" name="submit"/>
-
-            <input type="submit" value="Send" />
-        </form>
-    );
+        <div>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <Form />
+                </div>
+            </div>
+        </div>
+    )
 
     // const handleSubmit = (e) => {
     //     e.preventDefault();
@@ -127,6 +81,87 @@ function Contact() {
     //     document.getElementById('contactForm').reset();
     // }
 
+}
+
+function Form() {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,
+            process.env.REACT_APP_TEMPLATE_ID, 
+            e.target, 
+            process.env.REACT_APP_USER_ID)
+        .then((result) => {
+            console.log(result.text);
+            // toggle successful message
+            document.querySelector('.sent-successful').style.display = 'block';
+
+            setTimeout(() => {
+                document.querySelector('.sent-successful').style.display = 'none';
+            }, 3500);
+
+            e.target.reset();
+        }, (error) => {
+            console.log(error.text);
+            document.querySelector('.sent-error').style.display = 'block';
+            setTimeout(() => {
+                document.querySelector('.sent-error').style.display = 'none';
+            }, 3500);
+            // toggle unsuccesfful message sent
+            // do not reset form
+        });
+        // e.target.reset()
+    }
+
+    return (
+        <div className="col-6">
+            <h1>Contact Us</h1>
+            <form className="contact-form" onSubmit={sendEmail}>
+                
+                <div className="sent-successful">Message has been sent!</div>
+                <div className="sent-error">Error occured. Try again later.</div>
+
+                <div className="row">
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-12">
+                                <label className="form-label">Name</label>
+                            </div>
+                            <div className="col-12">
+                                <input className="input" type="text" name="name" />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-12">
+                                <label className="form-label">Email</label>
+                            </div>
+                            <div className="col-12">
+                                <input className="input" type="email" name="email" />
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="col-12">
+                        <div className="row">
+                            <div className="col-12">
+                                <label className="form-label">Message</label>
+                            </div>
+                            <div className="col-12">
+                                <textarea className="input-txt" type="text" name="message"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-12">
+                        <input className='input input-btn' type="submit" value="Send" />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default Contact;
