a0af39ab47d16582e83a2e925201ec89e19b54d7
patrickliu022 <patrickliu022@gmail.com>
2022-03-04 22:40:55 -0800

    reorganized files

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+// firebase.analytics();
 
 ReactDOM.render(
     <BrowserRouter>
