158037c7ed67d5a7a3187786ae41e16764c50fe5
Nathaniel Backus <nathanielbackus@me.com>
2022-06-10 11:15:49 -0700

    added image constraints, formatted text on donation page

--- frontend/app/src/pages/about/GetInvolved.js
+++ frontend/app/src/pages/about/GetInvolved.js
@@ -4,50 +4,54 @@ import "./css/GetInvolved.css"
 import { Button, CardImgOverlay, CardSubtitle, Col, Card, CardImg, CardTitle } from 'reactstrap';
 
 function GetInvolved() {
-    let pageContent = 
+    let pageContent =
         <div className="row justify-content-left">
             <div className="col-6">
-                Our ongoing work relies on the generosity of our supporters. 
-                If you are interested in making a donation, 
-                you can do so <a href="https://nelc.washington.edu/nelc-digital-support-our-work">here</a>.
-                Your investment in our work goes a long way! Thank you.
+                <div className="donation__text card card-body">
+                    <div>Our ongoing work relies on the generosity of our supporters.
+                        If you are interested in making a donation,
+                        you can do so <a href="https://nelc.washington.edu/nelc-digital-support-our-work">here</a>.
+                        Your investment in our work goes a long way! Thank you.</div>
+                </div>
             </div>
         </div>;
 
-    return(
+    return (
         <div className="container">
             <DonationBackdrop />
             {pageContent}
         </div>
     );
 }
-
+// header, image become scaling, include better text, lower background to fit footer and header/
 function DonationBackdrop() {
     return (
         <div className="row">
             <Col>
-                <Card inverse>
-                    <CardImg
-                        alt="donation image"
-                        src={donationBackdrop}
-                    />
-                    <CardImgOverlay>
-                        <CardTitle style={{color: "black", fontWeight: "750"}}>
-                            Get Involved with the Department of
-                            Near Eastern Languages and Civilizations
-                            <CardSubtitle>
-                                Supporting Text
-                            </CardSubtitle>
-                        </CardTitle>
-                    <Button>
-                        <a href="https://nelc.washington.edu/nelc-digital-support-our-work"
-                           className="get-involved-button"
-                        >
-                            Donate Now
-                        </a>
-                    </Button>
-                    </CardImgOverlay>
-                </Card>
+                <div className='cardimg__donation'>
+                    <Card inverse>
+                        <CardImg
+                            alt="donation image"
+                            src={donationBackdrop}
+                        />
+                        <CardImgOverlay>
+                            <CardTitle style={{ color: "black", fontWeight: "750" }}>
+                                Get Involved with the Department of
+                                Near Eastern Languages and Civilizations
+                                <CardSubtitle>
+                                    Supporting Text
+                                </CardSubtitle>
+                            </CardTitle>
+                            <Button>
+                                <a href="https://nelc.washington.edu/nelc-digital-support-our-work"
+                                    className="get-involved-button"
+                                >
+                                    Donate Now
+                                </a>
+                            </Button>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
             </Col>
         </div>
     );
