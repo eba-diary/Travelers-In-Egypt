0b0c3cf9da5c9262ef05557aebd50f461f1ebd8f
patrickliu022 <patrickliu022@gmail.com>
2022-09-08 21:43:14 -0700

    refactoring landing page

--- frontend/app/src/stand_alone_pages/Landing.js
+++ frontend/app/src/stand_alone_pages/Landing.js
@@ -1,19 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useContentfulLanding } from '../useContentful'
-
-import './css/Landing.css';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import aboutImage from '../img/aboutImage.jpeg';
-import goalCard1 from '../img/goalCard1.png';
-import goalCard2 from '../img/goalCard2.png';
-import goalCard3 from '../img/goalCard3.jpg';
-import arrows from '../img/goal_arrow_down.png';
-import background from '../img/eba_bg.png';
-import { CardImg, Col, Row, Card, CardTitle, CardText, CardBody, Button } from 'reactstrap';
-import { UncontrolledCarousel } from 'reactstrap';
-import projGoalsJSON from './ProjGoals.json';
-import { stringify } from 'nodemon/lib/utils';
-import { response } from 'express';
+import { Container, Col, Row, Card, CardBody, CardTitle, CardText} from 'reactstrap'
 
 export default function Landing(props) {
 
@@ -24,21 +11,34 @@ export default function Landing(props) {
     })
 
     useEffect(() => {
-        const featuredArticles = getFeaturedArticles()
-            .then(response => {
-                if (response) {
-                    return response.json()
-                }
-            }).catch((error) => {
-                console.log(`Error getting Landing page data: ${error}`)
-            })
-        setFeaturedArticles(featuredArticles)
+        getFeaturedArticles().then(response => setFeaturedArticles({articles: response.items}))
     }, [])
 
     return (
-        <div>
-            Test
-        </div>
+        <Container>
+            { featuredArticles.articles.map((entries, index) => {
+                const sliderCard = entries.fields.sliderCards.map((entries, index) => {
+                    return (
+                        <Card>
+                            <CardText>
+                                { entries.fields.title }
+                            </CardText>
+                            <CardBody>
+                                { entries.fields.description }
+                            </CardBody>
+                        </Card>
+                    )
+                })
+                return (
+                    <Row key={index}>
+                        <Col sm='3' md='1'>
+                            { sliderCard }
+                        </Col>
+                    </Row>
+                )})
+            }
+
+        </Container>
     )
 
 }
