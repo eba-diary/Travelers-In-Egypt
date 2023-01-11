a4c08b55f61e18a5bb8aeb1c4c419e339627fa08
Nathaniel Backus <nathanielbackus@me.com>
2023-01-10 19:00:54 -0800

    formatting changes

--- frontend/app/components/content/contact-form.js
+++ frontend/app/components/content/contact-form.js
@@ -1,9 +1,9 @@
 
 
 // import './css/Contact.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 // import 'firebase/database';
-// import emailjs from 'emailjs-com';
+import { useForm } from 'react-hook-form';
 import { useToast, VStack, HStack, Stack, Text, Button, Box, Textarea, Select, Input } from '@chakra-ui/react';
 
 
@@ -28,148 +28,145 @@ export default function ContactForm() {
 
     return (
         <VStack>
-                <VStack
+            <VStack
                 w='100%'
                 alignItems='left'
-                >
-                    <Stack>
-                        <Box
+            >
+                <Stack>
+                    <Box
                         paddingLeft='7%'
-                        >
-                            <Text
+                    >
+                        <Text
                             bg='#ffc55b'
                             display='inline-flex'
                             paddingLeft='5px'
                             paddingRight='10px'
                             fontWeight='600'
-                            >
+                        >
                             General Contact Information:
-                            </Text>
-                        </Box>
-                    </Stack>
-                    <Stack
+                        </Text>
+                    </Box>
+                </Stack>
+                <Stack
                     paddingLeft='8%'
-                    >
-                        <Box
+                >
+                    <Box
                         h='flex'
                         w='90%'
                         paddingLeft='5px'
                         marginBottom='15px'
                         borderLeft='3px solid #D08800'
                         bg='#ffe2ae'
-                        >
-                            <Text
+                    >
+                        <Text
                             paddingTop='5px'
                             paddingLeft='5px'
-                            >
-                                <Text>
-                                    <em style={{fontWeight: 600, fontStyle: 'initial'}}>Email: </em>
-                                    lorem@ipsum.com
-                                </Text>
+                        >
+                            <Text>
+                                <em style={{ fontWeight: 600, fontStyle: 'initial' }}>Email: </em>
+                                lorem@ipsum.com
                             </Text>
-                            <Text
+                        </Text>
+                        <Text
                             fontStyle='italic'
                             paddingBottom='5px'
-                            paddingLeft='5px'
-                            >
-                                Send any applications, inquiries, or concerns to this email.
-                            </Text>
-                        </Box>
-                    </Stack>
-                </VStack>
+                            paddingLeft='5px'>
+                            Send any applications, inquiries, or concerns to this email.
+                        </Text>
+                    </Box>
+                </Stack>
+            </VStack>
             <form
                 onSubmit={handleSubmit}
             >
                 <VStack
-                w='100%'
-                alignItems='left'
+                    w='100%'
+                    alignItems='left'
                 >
                     <Stack>
                         <Box
-                        paddingLeft='7%'
+                            paddingLeft='7%'
                         >
                             <Text
-                            bg='#ffc55b'
-                            display='inline-flex'
-                            paddingLeft='5px'
-                            paddingRight='10px'
-                            fontWeight='600'
-                            >
-                            Contact Request Form:
+                                bg='#ffc55b'
+                                display='inline-flex'
+                                paddingLeft='5px'
+                                paddingRight='10px'
+                                fontWeight='600'
+                                >
+                                Contact Request Form:
                             </Text>
                         </Box>
                     </Stack>
                     <Stack
-                    paddingLeft='8%'
-                    >
+                        paddingLeft='8%'>
                         <Box
-                        h='flex'
-                        w='90%'
-                        paddingLeft='5px'
-                        marginBottom='15px'
-                        borderLeft='3px solid #D08800'
-                        bg='#ffe2ae'
-                        >
+                            h='flex'
+                            w='90%'
+                            paddingLeft='5px'
+                            marginBottom='15px'
+                            borderLeft='3px solid #D08800'
+                            bg='#ffe2ae' >
                             <Stack direction='row'
-                            width='60%'
-                            alignItems='center'
-                            padding='10px 0px 5px 0px'>
+                                width='60%'
+                                alignItems='center'
+                                padding='10px 0px 5px 0px'>
                                 <Text
-                                flex={1}
-                                textAlign='center'
-                                height='fit-content'
-                                em style={{fontWeight: 600, fontStyle: 'initial'}}>
+                                    flex={1}
+                                    textAlign='center'
+                                    height='fit-content'
+                                    em style={{ fontWeight: 600, fontStyle: 'initial' }}>
                                     Your Name:
                                 </Text>
                                 <Input
-                                border='3px solid'
-                                borderColor='#ffc55b'
-                                bg='#FFFFFF'
-                                placeholder='Enter name here'
-                                flex={3}
-                                borderRadius='1px'
-                                onChange={(event) => {
-                                    setName(event.currentTarget.value)
-                                }}
+                                    border='3px solid'
+                                    borderColor='#ffc55b'
+                                    bg='#FFFFFF'
+                                    placeholder='Enter name here'
+                                    flex={3}
+                                    borderRadius='1px'
+                                    onChange={(event) => {
+                                        setName(event.currentTarget.value)
+                                    }}
                                 />
                             </Stack>
-                            <Stack 
-                            direction='row'
-                            width='60%'
-                            alignItems='center'
-                            padding='5px 0px 10px 0px'>
+                            <Stack
+                                direction='row'
+                                width='60%'
+                                alignItems='center'
+                                padding='5px 0px 10px 0px'>
                                 <Text
-                                flex={1}
-                                textAlign='center'
-                                height='fit-content'
-                                em style={{fontWeight: 600, fontStyle: 'initial'}}
+                                    flex={1}
+                                    textAlign='center'
+                                    height='fit-content'
+                                    em style={{ fontWeight: 600, fontStyle: 'initial' }}
                                 >
                                     Your Email:
                                 </Text>
                                 <Input
-                                border='3px solid'
-                                borderColor='#ffc55b'
-                                bg='#FFFFFF'
-                                placeholder='Enter Email here'
-                                flex={4}
-                                borderRadius='1px'
-                                onChange={(event) => {
-                                    setEmail(event.currentTarget.value)
-                                }}
+                                    border='3px solid'
+                                    borderColor='#ffc55b'
+                                    bg='#FFFFFF'
+                                    placeholder='Enter Email here'
+                                    flex={4}
+                                    borderRadius='1px'
+                                    onChange={(event) => {
+                                        setEmail(event.currentTarget.value)
+                                    }}
                                 />
                             </Stack>
                             <Text>
                                 Who are you trying to contact?
                             </Text>
                             <Select placeholder='Select option'
-                            bg='#FFFFFF'
-                            border='3px solid'
-                            borderColor='#ffc55b'
-                            borderRadius='1px'
-                            width='60%'
-                            onChange={(event) => {
-                                setSelect(event.currentTarget.value)
-                            }}>
+                                bg='#FFFFFF'
+                                border='3px solid'
+                                borderColor='#ffc55b'
+                                borderRadius='1px'
+                                width='60%'
+                                onChange={(event) => {
+                                    setSelect(event.currentTarget.value)
+                                }}>
                                 <option value='option1'>Option1</option>
                                 <option value='option2'>Option2</option>
                                 <option value='option3'>Option3</option>
@@ -180,33 +177,33 @@ export default function ContactForm() {
                                 Comment, Question, Concerns:
                             </Text>
                             <Textarea
-                            bg='#FFFFFF'
-                            border='3px solid'
-                            borderColor='#ffc55b'
-                            placeholder='Enter comments here'
-                            borderRadius='1px'
-                            onChange={(event) => {
-                                setName(event.currentTarget.value)
-                            }}
+                                bg='#FFFFFF'
+                                border='3px solid'
+                                borderColor='#ffc55b'
+                                placeholder='Enter comments here'
+                                borderRadius='1px'
+                                onChange={(event) => {
+                                    setResponse(event.currentTarget.value)
+                                }}
                             />
                             <HStack
-                            gap='20px'
-                            alignItems='flex-end'
-                            padding='25px'>
+                                gap='20px'
+                                alignItems='flex-end'
+                                padding='25px'>
                                 <Text fontStyle='italic'
-                                height='fit-content'
+                                    height='fit-content'
                                 >
-                                    The individual will be informed of your 
-                                    contact request once submitted. Your message 
-                                    will be forwarded to them, and you can expect 
+                                    The individual will be informed of your
+                                    contact request once submitted. Your message
+                                    will be forwarded to them, and you can expect
                                     a response within 2 - 3 business days.
                                 </Text>
                                 <Button
-                                height='20px'
-                                bgColor='#ffc55b'
-                                padding='8px 50px 8px 40px'
-                                borderRadius='4px'
-                                type='submit'>
+                                    height='20px'
+                                    bgColor='#ffc55b'
+                                    padding='8px 50px 8px 40px'
+                                    borderRadius='4px'
+                                    type='submit'>
                                     Submit
                                 </Button>
                             </HStack>
