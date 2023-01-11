d43c65890b77091b3fffe11f8e9ba477a21b3dba
Nathaniel Backus <nathanielbackus@me.com>
2023-01-10 19:52:38 -0800

    form changes

--- frontend/app/components/content/contact-form.js
+++ frontend/app/components/content/contact-form.js
@@ -3,8 +3,8 @@
 // import './css/Contact.css';
 import React, { useState } from 'react';
 // import 'firebase/database';
-import { useForm } from 'react-hook-form';
-import { useToast, VStack, HStack, Stack, Text, Button, Box, Textarea, Select, Input } from '@chakra-ui/react';
+import { useForm, FormProvider, Controller, FieldError } from 'react-hook-form';
+import { useToast, VStack, HStack, Stack, Text, Button, Box, Textarea, Select, Input, FormLabel, FormControl, FormErrorMessage} from '@chakra-ui/react';
 
 
 export default function ContactForm() {
@@ -12,8 +12,21 @@ export default function ContactForm() {
     const [email, setEmail] = useState('')
     const [select, setSelect] = useState('')
     const [response, setResponse] = useState('')
+
+    const methods = useForm({
+        mode: 'onBlur',
+        defaultValues: {
+            name: '',
+            email: '',
+            select: '',
+            response: ''
+        }
+    })
+
+    const { control, watch, handleSubmit, formState: { isValid } } = methods
+
     const toast = useToast()
-    const handleSubmit = () => {
+    const onSubmit = () => {
         toast({
             title: {
                 formName: name,
@@ -76,141 +89,174 @@ export default function ContactForm() {
                     </Box>
                 </Stack>
             </VStack>
-            <form
-                onSubmit={handleSubmit}
-            >
-                <VStack
-                    w='100%'
-                    alignItems='left'
+            <FormProvider {...methods}>
+                <form
+                    onSubmit={onSubmit}
                 >
-                    <Stack>
-                        <Box
-                            paddingLeft='7%'
-                        >
-                            <Text
-                                bg='#ffc55b'
-                                display='inline-flex'
+                    <VStack
+                        w='100%'
+                        alignItems='left'
+                    >
+                        <Stack>
+                            <Box
+                                paddingLeft='7%'
+                            >
+                                <Text
+                                    bg='#ffc55b'
+                                    display='inline-flex'
+                                    paddingLeft='5px'
+                                    paddingRight='10px'
+                                    fontWeight='600'
+                                >
+                                    Contact Request Form:
+                                </Text>
+                            </Box>
+                        </Stack>
+                        <Stack
+                            paddingLeft='8%'>
+                            <Box
+                                h='flex'
+                                w='90%'
                                 paddingLeft='5px'
-                                paddingRight='10px'
-                                fontWeight='600'
+                                marginBottom='15px'
+                                borderLeft='3px solid #D08800'
+                                bg='#ffe2ae'
+                            >
+                                <Stack direction='row'
+                                    width='60%'
+                                    alignItems='center'
+                                    padding='10px 0px 5px 0px'
                                 >
-                                Contact Request Form:
-                            </Text>
-                        </Box>
-                    </Stack>
-                    <Stack
-                        paddingLeft='8%'>
-                        <Box
-                            h='flex'
-                            w='90%'
-                            paddingLeft='5px'
-                            marginBottom='15px'
-                            borderLeft='3px solid #D08800'
-                            bg='#ffe2ae' >
-                            <Stack direction='row'
-                                width='60%'
-                                alignItems='center'
-                                padding='10px 0px 5px 0px'>
-                                <Text
-                                    flex={1}
-                                    textAlign='center'
-                                    height='fit-content'
-                                    em style={{ fontWeight: 600, fontStyle: 'initial' }}>
-                                    Your Name:
-                                </Text>
-                                <Input
-                                    border='3px solid'
-                                    borderColor='#ffc55b'
-                                    bg='#FFFFFF'
-                                    placeholder='Enter name here'
-                                    flex={3}
-                                    borderRadius='1px'
-                                    onChange={(event) => {
-                                        setName(event.currentTarget.value)
-                                    }}
-                                />
-                            </Stack>
-                            <Stack
-                                direction='row'
-                                width='60%'
-                                alignItems='center'
-                                padding='5px 0px 10px 0px'>
-                                <Text
-                                    flex={1}
-                                    textAlign='center'
-                                    height='fit-content'
-                                    em style={{ fontWeight: 600, fontStyle: 'initial' }}
-                                >
-                                    Your Email:
-                                </Text>
-                                <Input
-                                    border='3px solid'
-                                    borderColor='#ffc55b'
-                                    bg='#FFFFFF'
-                                    placeholder='Enter Email here'
-                                    flex={4}
-                                    borderRadius='1px'
-                                    onChange={(event) => {
-                                        setEmail(event.currentTarget.value)
-                                    }}
-                                />
-                            </Stack>
-                            <Text>
-                                Who are you trying to contact?
-                            </Text>
-                            <Select placeholder='Select option'
-                                bg='#FFFFFF'
-                                border='3px solid'
-                                borderColor='#ffc55b'
-                                borderRadius='1px'
-                                width='60%'
-                                onChange={(event) => {
-                                    setSelect(event.currentTarget.value)
-                                }}>
-                                <option value='option1'>Option1</option>
-                                <option value='option2'>Option2</option>
-                                <option value='option3'>Option3</option>
-                                <option value='option4'>Option4</option>
-                            </Select>
+                                    {/* <Text
+                                        flex={1}
+                                        textAlign='center'
+                                        height='fit-content'
+                                        em style={{ fontWeight: 600, fontStyle: 'initial' }}
+                                    >
+                                        Your Name:
+                                    </Text>
+                                    <Input
+                                        border='3px solid'
+                                        borderColor='#ffc55b'
+                                        bg='#FFFFFF'
+                                        placeholder='Enter name here'
+                                        flex={3}
+                                        borderRadius='1px'
+                                        onChange={(event) => {
+                                            setName(event.currentTarget.value)
+                                        }}
+                                    /> */}
+                                    <Controller
+                                        name='name'
+                                        control={control}
+                                        rules={{
+                                            validate: (data) => {
+                                                if (data.length === 0) return 'Please enter a name'
+                                                return true
+                                            }
+                                        }}
+                                        render={({field: {onChange, value}, fieldState: {error}}) => (
+                                            <FormControl isInvalid={!!error}>
+                                                <FormLabel>Name</FormLabel>
+                                                <Input
+                                                    onChange={onChange}
+                                                    value={value}
+                                                />
+                                                <FormErrorMessage>
+                                                    <Text>{error.message.length == 0 ? '' : error}</Text>
+                                                </FormErrorMessage>
 
-                            <Text>
-                                Comment, Question, Concerns:
-                            </Text>
-                            <Textarea
-                                bg='#FFFFFF'
-                                border='3px solid'
-                                borderColor='#ffc55b'
-                                placeholder='Enter comments here'
-                                borderRadius='1px'
-                                onChange={(event) => {
-                                    setResponse(event.currentTarget.value)
-                                }}
-                            />
-                            <HStack
-                                gap='20px'
-                                alignItems='flex-end'
-                                padding='25px'>
-                                <Text fontStyle='italic'
-                                    height='fit-content'
+                                            </FormControl>
+                                        )}
+                                    />
+                                </Stack>
+                                <Stack
+                                    direction='row'
+                                    width='60%'
+                                    alignItems='center'
+                                    padding='5px 0px 10px 0px'
                                 >
-                                    The individual will be informed of your
-                                    contact request once submitted. Your message
-                                    will be forwarded to them, and you can expect
-                                    a response within 2 - 3 business days.
+                                    <Text
+                                        flex={1}
+                                        textAlign='center'
+                                        height='fit-content'
+                                        em style={{ fontWeight: 600, fontStyle: 'initial' }}
+                                    >
+                                        Your Email:
+                                    </Text>
+                                    <Input
+                                        border='3px solid'
+                                        borderColor='#ffc55b'
+                                        bg='#FFFFFF'
+                                        placeholder='Enter Email here'
+                                        flex={4}
+                                        borderRadius='1px'
+                                        onChange={(event) => {
+                                            setEmail(event.currentTarget.value)
+                                        }}
+                                    />
+                                </Stack>
+                                <Text>
+                                    Who are you trying to contact?
                                 </Text>
-                                <Button
-                                    height='20px'
-                                    bgColor='#ffc55b'
-                                    padding='8px 50px 8px 40px'
-                                    borderRadius='4px'
-                                    type='submit'>
-                                    Submit
-                                </Button>
-                            </HStack>
-                        </Box>
-                    </Stack>
-                </VStack>
-            </form>
+                                <Select placeholder='Select option'
+                                    bg='#FFFFFF'
+                                    border='3px solid'
+                                    borderColor='#ffc55b'
+                                    borderRadius='1px'
+                                    width='60%'
+                                    onChange={(event) => {
+                                        setSelect(event.currentTarget.value)
+                                    }}
+                                >
+                                    <option value='option1'>Option1</option>
+                                    <option value='option2'>Option2</option>
+                                    <option value='option3'>Option3</option>
+                                    <option value='option4'>Option4</option>
+                                </Select>
+
+                                <Text>
+                                    Comment, Question, Concerns:
+                                </Text>
+                                <Textarea
+                                    bg='#FFFFFF'
+                                    border='3px solid'
+                                    borderColor='#ffc55b'
+                                    placeholder='Enter comments here'
+                                    borderRadius='1px'
+                                    onChange={(event) => {
+                                        setResponse(event.currentTarget.value)
+                                    }}
+                                />
+                                <HStack
+                                    gap='20px'
+                                    alignItems='flex-end'
+                                    padding='25px'
+                                >
+                                    <Text
+                                        fontStyle='italic'
+                                        height='fit-content'
+                                    >
+                                        The individual will be informed of your
+                                        contact request once submitted. Your message
+                                        will be forwarded to them, and you can expect
+                                        a response within 2 - 3 business days.
+                                    </Text>
+                                    <Button
+                                        height='20px'
+                                        bgColor='#ffc55b'
+                                        padding='8px 50px 8px 40px'
+                                        borderRadius='4px'
+                                        type='submit'
+                                    >
+                                        Submit
+                                    </Button>
+                                </HStack>
+                            </Box>
+                        </Stack>
+                    </VStack>
+                </form>
+            </FormProvider>
         </VStack>
     )
 }
