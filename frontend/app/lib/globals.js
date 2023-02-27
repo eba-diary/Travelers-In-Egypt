b4b9bafb5891c74415d71f201e34d7deefd2ea6a
PatrickLiu022 <patrickliu022@gmail.com>
2023-02-26 23:41:28 -0800

    finished deploying flask api pt 2

--- frontend/app/lib/globals.js
+++ frontend/app/lib/globals.js
@@ -1,1 +1,2 @@
-export const API_BASE_URI = 'cdn.contentful.com'
+export const CMS_BASE_URI = 'cdn.contentful.com'
+export const API_BASE_URI = process.env.NODE_ENV === 'dev' ? 'http://localhost:8080' : process.env.API_BASE_URI
