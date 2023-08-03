440cc933dc482c776d6373930e9ca13ef779e647
PatrickLiu022 <patrickliu022@gmail.com>
2023-08-03 00:41:15 -0700

    trying different approach for hmt cors part 4

--- tools/markup-tool/app.py
+++ tools/markup-tool/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template
 from gevent.pywsgi import WSGIServer
 from ner.flair_ner import tag_entities
 from tei.assemble_tei import create_header, create_xml, create_body
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
@@ -12,36 +12,41 @@ CORS(
     app, 
     supports_credentials=True, 
     methods=['GET', 'POST', 'OPTIONS'], 
-    resources={
-        r"/*": {
-            "origins": [
-                "http://localhost:3000", 
-                "https://travelers-in-egypt-preview.vercel.app", 
-                "https://travelers-in-egypt.vercel.app"
-            ]
-        }
-    }
+    # resources={
+    #     r"/*": {
+    #         "origins": [
+    #             "http://localhost:3000", 
+    #             "https://travelers-in-egypt-preview.vercel.app", 
+    #             "https://travelers-in-egypt.vercel.app"
+    #         ]
+    #     }
+    # }
 )
 
 
-origins = [
-    "http://localhost:3000", 
-    "https://travelers-in-egypt-preview.vercel.app", 
-    "https://travelers-in-egypt.vercel.app"
-]
+# origins = [
+#     "http://localhost:3000", 
+#     "https://travelers-in-egypt-preview.vercel.app", 
+#     "https://travelers-in-egypt.vercel.app"
+# ]
 
-@app.after_request
-def add_cors_headers(response):
-    r = request.referrer[:-1]
-    if r in origins:
-        response.headers.add('Access-Control-Allow-Origin', r)
-        response.headers.add('Access-Control-Allow-Credentials', 'true')
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
-        response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
-        response.headers.add('Access-Control-Allow-Headers', 'Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-    return response
+# @app.after_request
+# def add_cors_headers(response):
+#     r = request.referrer
+#     if not r:
+#         r = origins[2]
+#     else:
+#         r = r[:-1]
+
+#     if r in origins:
+#         response.headers.add('Access-Control-Allow-Origin', r)
+#         response.headers.add('Access-Control-Allow-Credentials', 'true')
+#         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+#         response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+#         response.headers.add('Access-Control-Allow-Headers', 'X-Requested-With')
+#         response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+#         response.headers.add('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+#     return response
 
 @app.route('/')
 def index():
@@ -59,6 +64,7 @@ def output():
 
 
 @app.route('/', methods=['POST'])
+@cross_origin()
 def submit_text():
     title = request.form['teiHeaderTitle']
     author = request.form['teiHeaderAuthor']
