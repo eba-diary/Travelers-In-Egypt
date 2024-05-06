efed7b6e070d251cd329ff5af1678fafa25f4d78
Emma Fritzberg <em.fritzberg@gmail.com>
2024-05-06 16:18:29 -0700

    lint fixes

--- frontend/app/lib/hooks/use-viewport-dimensions.ts
+++ frontend/app/lib/hooks/use-viewport-dimensions.ts
@@ -16,22 +16,20 @@ export const useViewportDimensions = (): ViewportDimensionHook => {
 		y: 0
 	});
 
-	if (typeof window !== 'undefined') {
-		const updateViewport = debounce((x: number, y: number) => {
-			setViewportDimensions({ x, y });
-		}, 50);
+	const updateViewport = debounce((x: number, y: number) => {
+		setViewportDimensions({ x, y });
+	}, 50);
 
-		useEffect(() => {
-			const { x, y } = { x: window.innerWidth, y: window.innerHeight }
-			updateViewport(x, y)
+	useEffect(() => {
+		const { x, y } = { x: window?.innerWidth, y: window?.innerHeight }
+		updateViewport(x, y)
 
-			window.addEventListener("resize", () => updateViewport(x, y))
+		window?.addEventListener("resize", () => updateViewport(x, y))
 
-			return () => {
-				window.removeEventListener("resize", () => updateViewport(x, y))
-			}
-		}, [window.innerWidth, window.innerHeight]);
-	}
+		return () => {
+			window?.removeEventListener("resize", () => updateViewport(x, y))
+		}
+	}, []);
 
 	return {
 		viewportDimensions,
