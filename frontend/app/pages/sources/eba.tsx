694863c050149367f56e772df5a6212b1e001f02
Emma Fritzberg <em.fritzberg@gmail.com>
2024-05-07 17:28:03 -0700

    Update eba.tsx

--- frontend/app/pages/sources/eba.tsx
+++ frontend/app/pages/sources/eba.tsx
@@ -9,13 +9,11 @@ export default function EBA() {
 	const [volume, setVolume] = useState<number>(1);
 
 	const readeriFrameHtml = `
-		<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-			<iframe
-				src="https://ec2-54-188-209-13.us-west-2.compute.amazonaws.com/exist/apps/eba/volumes/volume${volume}.xml"
-				width="100%"
-				height="550px"
-			/>
-		</meta>
+		<iframe
+			src="https://ec2-54-188-209-13.us-west-2.compute.amazonaws.com/exist/apps/eba/volumes/volume${volume}.xml"
+			width="100%"
+			height="550px"
+		/>
 	`;
 
 	return (
